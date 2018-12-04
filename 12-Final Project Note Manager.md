fb08dc5f7986707f37364eac532f3946f7037d6d
Eva Sanchez Guerrero <acalpixca@gmail.com>
2018-12-04 10:57:12 +0100

    first half of final project

--- 12-Final Project Note Manager.md
+++ 12-Final Project Note Manager.md
@@ -1,13 +1,13 @@
 # Final Project: Create a markdown editor for personal notes
 
 Start installing the framework with:
-``` 
+```
 create-react-app note-manager
 // to launch it, do:
 cd note-manager
 yarn Start
 ```
-Then we add a styling manager called Miligram, more details here https://milligram.io
+Then we add a styling manager called Milligram, more details here https://milligram.io
 ```
 yarn add milligram
 ```
@@ -15,3 +15,12 @@ Then we add a simple md editor package that already exists :-)
 ```
 yarn add react-simplemde-editor
 ```
+We add the milligram stylesheet to the \<link\> block of index.html in the public folder.
+
+We edit App.js (the component where the whole app is contained) and modify it to:
+* Import Milligram.
+* Invoke the (yet to be done) components Editor and Sidebar from the main div.
+* Import Editor and Sidebar.
+
+Then we create the components folder inside of src.
+We create Sidebar.js and Editor.js in the components folder.
--- note-manager/src/App.js
+++ note-manager/src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import 'milligram';
+
+import Sidebar from './components/Sidebar';
+import Editor from './components/Editor';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.change = this.change.bind(this);
+    this.addNote = this.addNote.bind(this);
+  }
+
+  addNote(){
+    console.log('added note!');
+  }
+
+  change(){
+    console.log('changed!');
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+      <h1>marky mark</h1>
+        <div className="row">
+          <Sidebar addNote={this.addNote}/>
+          <Editor change={this.change} />
+          </div>
       </div>
     );
   }
